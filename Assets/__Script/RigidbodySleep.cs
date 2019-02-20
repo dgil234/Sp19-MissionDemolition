6f366f770ad680d0d28499689a2f4c7a1e6a448c
dgil234 <dgil1026@gmail.com>
2019-02-20 00:44:16 -0500

    Hitting Goal done

---
+++ Assets/__Script/RigidbodySleep.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour {
+
+  	// Use this for initialization
+	void Start () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null) rb.Sleep();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
